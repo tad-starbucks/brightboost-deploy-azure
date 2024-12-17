a6b3f9ffe80ce66b0164971299d92b65f024d238
Tad Lispy <tadeusz@lazurski.pl>
2024-12-17 17:00:38 +0100

    Change something to see if it triggers the CI

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Hello, Azure DevOps! What a pity it doesn't work.`);
+  res.send(`Hello, Starbucks`);
 });
 
 app.listen(port, () => {
